adcff3f08d706a84e36c641251c8edcb4f468fcb
hellosamkim <sam.mt.kim@gmail.com>
2015-02-04 13:38:07 -0500

    Fixed up front page with new font styling and colours are more randomized

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,7 +2,7 @@ class Day < ActiveRecord::Base
   has_many :day_tasks
 
   def bg_colour
-    generator = ColorGenerator.new saturation: rand(0.3..0.5), lightness: rand(0.8..0.9)
+    generator = ColorGenerator.new saturation: rand(0.291..0.635), lightness: rand(0.658..0.912), value: 1.0
     generator.create_hex
   end
 end
