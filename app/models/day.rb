dd443d90e8b6ad5e88ff94ec96036a7905f6808f
hellosamkim <sam.mt.kim@gmail.com>
2015-02-04 14:46:48 -0500

    Cleared out unnecessary files and started working on frontend for days pages

--- app/models/day.rb
+++ app/models/day.rb
@@ -5,4 +5,9 @@ class Day < ActiveRecord::Base
     generator = ColorGenerator.new saturation: rand(0.291..0.635), lightness: rand(0.658..0.912), value: 1.0
     generator.create_hex
   end
+
+  def bg_colour2
+    generator = ColorGenerator.new saturation: rand(0.3..0.4), lightness: rand(0.8..0.912), value: 1.0
+    generator.create_hex
+  end
 end
