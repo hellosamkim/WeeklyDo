9930839a9669a72d88bf5e8d4e7b4d0994b61b15
hellosamkim <sam.mt.kim@gmail.com>
2015-02-07 14:11:23 -0500

    Fixed bug to display 'All tasks are finished' when complete

--- app/models/day.rb
+++ app/models/day.rb
@@ -10,4 +10,10 @@ class Day < ActiveRecord::Base
     generator = ColorGenerator.new saturation: rand(0.3..0.4), lightness: rand(0.8..0.912), value: 1.0
     generator.create_hex
   end
+
+  def all_done
+    self.day_tasks.all? do |day_task|
+      day_task.finished_at.to_s.length >= 3
+    end
+  end
 end
